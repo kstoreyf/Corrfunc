02f4e2e55fd81becbea05accc325656befb3c7eb
Kate Storey-Fisher <kstoreyfisher@gmail.com>
2020-07-30 22:58:17 -0400

    fixed common.mk python variable

--- common.mk
+++ common.mk
@@ -19,7 +19,6 @@ CLINK ?=
 ## Set the python command (supply the full path to python you want to
 ## use, if different from directly calling `python` on the shell,
 ## as can be the case if python is set via an alias)
-#PYTHON:=/home/users/ksf293/miniconda3/bin/python3
 PYTHON:=python
 
 ## Important note -> if you directly call /some/path/to/python
