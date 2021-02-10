ff2b31118fe9a2ed9e9093d590907be9e77f15d1
Kate Storey-Fisher <kstoreyfisher@gmail.com>
2021-02-10 10:58:39 -0500

    update bases docstrings

--- Corrfunc/bases.py
+++ Corrfunc/bases.py
@@ -10,37 +10,61 @@ Helper routines for basis functions
 # Spline basis #
 ################
 
-def spline_bases(rmin, rmax, nbins, projfn, ncont=1000, order=None):
+def spline_bases(rmin, rmax, projfn, ncomponents, ncont=2000, order=3):
+    '''
+    Compute a set of spline basis functions for the given order.
 
-    if order is None:
-        print("No order given, defaulting to 1 (linear)")
-    
-    if nbins<order*2:
-        # does it have to be 2*order + 1? seems fine for piecewise, but for higher orders?
-        raise ValueError("nbins must be at least twice the order")
+    Parameters
+    ----------
+    rmin : double
+        Minimum r-value for integration 
 
-    kvs = _get_knot_vectors(rmin, rmax, nbins, order)
+    rmax : double
+        Maximum r-value for integration 
+
+    projfn : string, default=None
+       Filename of projection file if necessary
+
+    ncomponents : int
+       Number of components (basis functions)
+
+    ncont : int, default=1000
+       Number of continuous r-values at which to write the basis function file
+
+    order : int, default=3
+       Order of spline to use; default is cubic spline (order=3)
+
+    Returns
+    -------
+    bases: array-like, double
+        2-d array of basis function values; first column is r-values
+
+    '''
+    if ncomponents<order*2:
+        raise ValueError("ncomponents must be at least twice the order")
+
+    kvs = _get_knot_vectors(rmin, rmax, ncomponents, order)
     rcont = np.linspace(rmin, rmax, ncont)
-    bases = np.empty((nbins+1, ncont))
+    bases = np.empty((ncomponents+1, ncont))
     bases[0,:] = rcont
-    for n in range(nbins):
+    for n in range(ncomponents):
         kv = kvs[n]
         b = BSpline.basis_element(kv)
         bases[n+1,:] = [b(r) if kv[0]<=r<=kv[-1] else 0 for r in rcont]
 
     np.savetxt(projfn, bases.T)
-    return projfn
+    return bases
 
 
-def _get_knot_vectors(rmin, rmax, nbins, order):
+def _get_knot_vectors(rmin, rmax, ncomponents, order):
     nknots = order+2
-    kvs = np.empty((nbins, nknots))
-    width = (rmax-rmin)/(nbins-order)
+    kvs = np.empty((ncomponents, nknots))
+    width = (rmax-rmin)/(ncomponents-order)
     for i in range(order):
         val = i+1
         kvs[i,:] = np.concatenate((np.full(nknots-val, rmin), np.linspace(rmin+width, rmin+width*val, val)))
-        kvs[nbins-i-1] = np.concatenate((np.linspace(rmax-width*val, rmax-width, val), np.full(nknots-val, rmax)))
-    for j in range(nbins-2*order):
+        kvs[ncomponents-i-1] = np.concatenate((np.linspace(rmax-width*val, rmax-width, val), np.full(nknots-val, rmax)))
+    for j in range(ncomponents-2*order):
         idx = j+order
         kvs[idx] = rmin+width*j + np.arange(0,nknots)*width                     
     return kvs
@@ -50,10 +74,53 @@ def _get_knot_vectors(rmin, rmax, nbins, order):
 # BAO basis #
 #############
 
-def bao_bases(rmin, rmax, projfn, cosmo_base, ncont=1000, redshift=0, 
-                dalpha=0.01, alpha_model=1.0, bias=1.0, k0=0.1):
-    if not cosmo_base:
-        raise ValueError("Must pass cosmo_base!")
+def bao_bases(rmin, rmax, projfn, cosmo_base=None, ncont=1000, redshift=0.0, 
+                alpha_model=1.0, dalpha=0.001, bias=1.0, k0=0.1):
+    '''
+    Compute the 5-component BAO basis functions based on a cosmological model and 
+    linearized around the scale dilation parameter alpha.
+
+    Parameters
+    ----------
+    rmin : double
+        Minimum r-value for integration 
+
+    rmax : double
+        Maximum r-value for integration 
+
+    projfn : string, default=None
+        Filename of projection file if necessary
+
+    cosmo_base : nbodykit cosmology object, default=nbodykit.cosmology.Planck15
+        Cosmology object for the BAO model.
+
+    ncont : int, default=1000
+        Number of continuous r-values at which to write the basis function file
+
+    redshift : double, default=0.0
+        Redshift at which to compute power spectrum
+
+    alpha_model : double, default=1.0
+        The alpha (scale dilation parameter) at which to compute the model (alpha=1.0 is no scale shift)
+
+    dalpha : double, default=0.001
+        The change in alpha (scale dilation parameter) used to calculate the numerical partial derivative
+    
+    bias : double, default=1.0
+        The bias parameter by which to scale the model amplitude (bias=1.0 indicates no bias)
+
+    k0 : double, default=0.1
+        The initial magnitude of the derivative term 
+
+    Returns
+    -------
+    bases: array-like, double
+        2-d array of basis function values; first column is r-values
+
+    '''
+    if cosmo_base is None:
+        print("cosmo_base not provided, defaulting to Planck cosmology (nbodykit.cosmology.Planck15)")
+        cosmo_base = nbodykit.cosmology.Planck15
 
     Plin = cosmology.LinearPower(cosmo_base, redshift, transfer='EisensteinHu')
     CF = cosmology.correlation.CorrelationFunction(Plin)
@@ -71,7 +138,7 @@ def bao_bases(rmin, rmax, projfn, cosmo_base, ncont=1000, redshift=0,
     
     np.savetxt(projfn, bases.T)
     nprojbins = bases.shape[0]-1
-    return nprojbins, projfn
+    return bases
 
 
 def _get_bao_components(s, cf_func, dalpha, alpha, k0=0.1):   
